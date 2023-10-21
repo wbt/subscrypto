f6759b1212eadf03d6bd52aef3d0748cfc889178
Lawrence Ley <lawrence.ley@contextsolutions.ca>
2023-10-21 17:38:47 -0400

    - added new aws lambda functions for new subscriptions and usage updates

---
+++ aws/lambda-functions/usage/lambda_function.py
@@ -0,0 +1,42 @@
+import json
+import os, re, base64
+import boto3
+
+def lambda_handler(event, context):
+    
+    mypage = page_router(event['httpMethod'],event['queryStringParameters'],event['body'])
+    
+    return mypage
+
+
+def page_router(httpmethod,querystring,formbody):
+
+    if httpmethod == 'GET':
+        htmlFile = open('usage.html', 'r')
+        htmlContent = htmlFile.read()
+        return {
+        'statusCode': 200, 
+        'headers': {"Content-Type":"text/html"},
+        'body': htmlContent
+        }
+    
+    if httpmethod == 'POST':
+        
+        insert_record(formbody)
+        
+        htmlFile = open('status.html', 'r')
+        htmlContent = htmlFile.read()
+        return {
+        'statusCode': 200, 
+        'headers': {"Content-Type":"text/html"},
+        'body': htmlContent
+        }    
+
+def insert_record(formbody):
+    
+    formbody = formbody.replace("=", "' : '")
+    formbody = formbody.replace("&", "', '")
+    formbody = "INSERT INTO usage value {'" + formbody +  "'}"
+    
+    client = boto3.client('dynamodb')
+    client.execute_statement(Statement= formbody)
