9a2ba29743d37d981403f1572601a95148cde32b
WBT <WBT@users.noreply.github.com>
2023-10-22 05:25:18 -0400

    Add withdraw option for merchant

---
+++ packages/nextjs/components/example-ui/withdrawCredits.tsx
@@ -0,0 +1,38 @@
+import { ActionButton } from "./actionButton";
+import { formatEther } from "viem";
+import { useAccount } from "wagmi";
+import { useScaffoldContractRead, useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
+
+export const WithdrawCredits = () => {
+  const { address } = useAccount();
+  const { data: balance, isLoading: balanceIsLoading } = useScaffoldContractRead({
+    contractName: "SubscryptoToken",
+    functionName: "balanceOf",
+    args: [address],
+  });
+  const { writeAsync, isLoading: withdrawalIsLoading } = useScaffoldContractWrite({
+    contractName: "SubscryptoToken",
+    functionName: "withdraw",
+    //@ts-ignore not sure why it's not picking up on this function's presence.
+    args: [],
+    onBlockConfirmation: txnReceipt => {
+      console.log("📦 Transaction blockHash", txnReceipt.blockHash);
+    },
+  });
+  let balanceText = "Your balance is loading.";
+  if (!balanceIsLoading) {
+    if (typeof balance === "undefined") {
+      balanceText = "You have no currently recognizable free balance.";
+    } else {
+      balanceText = "You have " + formatEther(balance) + " credits available for withdrawal.";
+    }
+  }
+
+  return (
+    <li className="py-8 px-5 border border-primary rounded-xl flex flex-col m-5">
+      <span className="text-xl my-4">Withdraw credits:</span>
+      {balanceText}
+      <ActionButton onClick={writeAsync} text={"Withdraw all"} isLoading={balanceIsLoading || withdrawalIsLoading} />
+    </li>
+  );
+};
--- packages/nextjs/pages/merchant.tsx
+++ packages/nextjs/pages/merchant.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import { useAccount } from "wagmi";
 import { MetaHeader } from "~~/components/MetaHeader";
 import { TierListing } from "~~/components/example-ui/TierListing";
+import { WithdrawCredits } from "~~/components/example-ui/withdrawCredits";
 
 const MerchantSetup: NextPage = () => {
   const { address } = useAccount();
@@ -12,6 +13,7 @@ const MerchantSetup: NextPage = () => {
         description="Allows the maker of an API to set up offerings to sell access for cryptocurrency payments."
       />
       <TierListing showOfferedStatus={true} merchant={address} />
+      <WithdrawCredits />
     </>
   );
 };
