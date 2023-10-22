9dce459f8d0e72304aa8daf188a3f0be0f16e0c5
WBT <WBT@users.noreply.github.com>
2023-10-22 02:24:27 -0400

    Drop data-theme attribute

--- packages/nextjs/pages/merchant.tsx
+++ packages/nextjs/pages/merchant.tsx
@@ -9,7 +9,7 @@ const MerchantSetup: NextPage = () => {
         title="API Merchant: Set up tiers"
         description="Allows the maker of an API to set up offerings to sell access for cryptocurrency payments."
       />
-      <div className="grid lg:grid-cols-2 flex-grow" data-theme="exampleUi">
+      <div className="grid lg:grid-cols-2 flex-grow">
         <TierListing showOfferedStatus={true} />
       </div>
     </>
