042461b7e24cd82556dc0a61d3917f19d0d93935
WBT <WBT@users.noreply.github.com>
2023-10-22 01:50:44 -0400

    Add buttons to set active status

--- packages/nextjs/components/example-ui/activeStatus.tsx
+++ packages/nextjs/components/example-ui/activeStatus.tsx
@@ -1,4 +1,5 @@
-import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
+import { ActionButton } from "./actionButton";
+import { useScaffoldContractRead, useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 
 export const ActiveStatus = (props: { merchant: string; tierIndex: bigint }) => {
   const { data: isActivelyOffered, isLoading: activeStatusIsLoading } = useScaffoldContractRead({
@@ -7,13 +8,53 @@ export const ActiveStatus = (props: { merchant: string; tierIndex: bigint }) => 
     args: [props.merchant, props.tierIndex],
   });
 
+  const { writeAsync: setActivelyOfferedTrue, isLoading: writeTrueIsLoading } = useScaffoldContractWrite({
+    contractName: "SubscryptoToken",
+    functionName: "setTierisActivelyOffered",
+    args: [props.tierIndex, true],
+    onBlockConfirmation: txnReceipt => {
+      console.log("📦 Transaction blockHash", txnReceipt.blockHash);
+    },
+  });
+  //TODO: DRY above + below
+  const { writeAsync: setActivelyOfferedFalse, isLoading: writeFalseIsLoading } = useScaffoldContractWrite({
+    contractName: "SubscryptoToken",
+    functionName: "setTierisActivelyOffered",
+    args: [props.tierIndex, false],
+    onBlockConfirmation: txnReceipt => {
+      console.log("📦 Transaction blockHash", txnReceipt.blockHash);
+    },
+  });
+
+  const anythingIsLoading = activeStatusIsLoading || writeTrueIsLoading || writeFalseIsLoading;
+
   if (activeStatusIsLoading) {
     return <>Checking to see if this is actively offered.</>;
   } else {
     if (isActivelyOffered) {
-      return <>Actively offered</>;
+      return (
+        <>
+          Actively offered.
+          <br />
+          <ActionButton
+            onClick={setActivelyOfferedFalse}
+            isLoading={anythingIsLoading}
+            text={"Stop offering this tier to new clients"}
+          />
+        </>
+      );
     } else {
-      return <>Not actively offered.</>;
+      return (
+        <>
+          Not actively offered.
+          <br />
+          <ActionButton
+            onClick={setActivelyOfferedTrue}
+            isLoading={anythingIsLoading}
+            text={"Offer this tier to new clients"}
+          />
+        </>
+      );
     }
   }
 };
